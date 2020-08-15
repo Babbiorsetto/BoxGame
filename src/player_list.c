c0820276081e98b54299ad8aaf819a44fc56d84d
Alessandro Rubino <aleruby97@hotmail.it>
2020-08-15 17:21:34 +0200

    Modify player list files structure
    Change some signatures, move doc comments to header.

--- src/player_list.c
+++ src/player_list.c
@@ -20,35 +20,16 @@ struct player_list_iterator_t
     struct player_node_t *current;
 };
 
-/*
-* Allocates memory for an iterator on the given list.
-*
-* @param list The list on which the new iterator cycles
-* @return A pointer to the newly created iterator
-*/
-struct player_list_iterator_t *create_iterator(struct player_list_t *list)
+int player_list_iterator_create(struct player_list_t *list, struct player_list_iterator_t *iterator)
 {
     //TODO
 }
 
-/*
-* Frees the memory used by the given iterator.
-*
-* @param iterator The iterator to destroy
-* @return 0 if the iterator was destroyed successfully, non-zero otherwise
-*/
-int destroy_iterator(struct player_list_iterator_t *iterator)
+int player_list_iterator_destroy(struct player_list_iterator_t *iterator)
 {
     //TODO
 }
 
-/*
-* Advances iterator, returns the current player alias and signals whether the returned element was the last in one atomic operation.
-*
-* @param iterator The iterator to advance
-* @param last After the call to next, it points to 1 if the element returned by iterator was the last element of the list, 0 otherwise
-* @return The player at the iterator's current position
-*/
 struct player_alias_t *player_list_iterator_next(struct player_list_iterator_t *iterator, int *last)
 {
     /*TODO body*/
--- src/player_list.h
+++ src/player_list.h
@@ -1,3 +1,39 @@
+#include "player_alias.h"
+
 struct player_list_t;
 struct player_node_t;
 struct player_list_iterator_t;
+
+/*
+* Allocates memory for an iterator on the given list.
+*
+* @param list The list on which the new iterator cycles
+* @param iterator Memory location that will contain the new iterator, if the call to create succeded
+* @return 0 if the iterator was created successfully, non-zero otherwise
+*/
+int player_list_iterator_create(struct player_list_t *list, struct player_list_iterator_t *iterator);
+
+/*
+* Frees the memory used by the given iterator.
+*
+* @param iterator The iterator to destroy
+* @return 0 if the iterator was destroyed successfully, non-zero otherwise
+*/
+int player_list_iterator_destroy(struct player_list_iterator_t *iterator);
+
+/*
+* Advances iterator, returns the current player alias and signals whether the returned element was the last in one atomic operation.
+*
+* @param iterator The iterator to advance
+* @param last After the call to next, it points to 1 if the element returned by iterator was the last element of the list, 0 otherwise
+* @return The player at the iterator's current position
+*/
+struct player_alias_t *player_list_iterator_next(struct player_list_iterator_t *iterator, int *last);
+
+/*
+* Allocates memory for a player list and initializes it.
+*
+* @param list Memory location that will contain the new list, if the call succeded
+* @return 0 if successful, non-zero otherwise
+*/
+int player_list_create(struct player_list_t *list);
