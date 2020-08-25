18ea32f85d5ebf7d04f614580ef4979762d65d29
Alessandro Rubino <aleruby97@hotmail.it>
2020-08-25 19:04:58 +0200

    add personal_map skeleton

---
+++ src/personal_map.c
@@ -0,0 +1,42 @@
+#include "stdlib.h"
+#include "game_map.h"
+struct personal_map_t
+{
+    char *data;
+    struct game_map_t *reference;
+};
+
+int personal_map_create(struct personal_map_t **map, struct game_map_t *reference)
+{
+    if (reference == NULL)
+    {
+        return -1;
+    }
+    
+    *map = malloc(sizeof(struct personal_map_t));
+    if (map == NULL)
+    {
+        return -1;
+    }
+    (*map)->data = malloc(game_map_getWidth(reference) * game_map_getHeight(reference) * sizeof(char));
+    if ((*map)->data == NULL)
+    {
+        free(*map);
+        return -1;
+    }
+
+    (*map)->reference = reference;
+    return 0;
+}
+
+int personal_map_destroy(struct personal_map_t *map)
+{
+    if (map == NULL)
+    {
+        return -1;
+    }
+
+    free(map->data);
+    free(map);
+    return 0;
+}
---
+++ src/personal_map.h
@@ -0,0 +1,4 @@
+#ifndef PERSONAL_MAP_H
+#define PERSONAL_MAP_H
+
+#endif
